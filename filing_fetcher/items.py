b4899d62c8e490859ce41ac26f300ac245dffc6f
Philip Lee <philip.l.lee@ons.gov.uk>
2019-04-24 11:51:34 +0100

    Output item even if paper filing not found

--- filing_fetcher/items.py
+++ filing_fetcher/items.py
@@ -9,11 +9,20 @@ import scrapy
 
 
 class FilingItem(scrapy.Item):
+    # From the BasicCompanyInformation file
+    company_number = scrapy.Field()
+    company_status = scrapy.Field()
+    incorporation_date = scrapy.Field()
+    accounts_next_due_date = scrapy.Field()
+    accounts_last_made_up_date = scrapy.Field()
+    accounts_account_category = scrapy.Field()
+
+    # From the filing-history
     pages = scrapy.Field()
     date = scrapy.Field()
     type = scrapy.Field()
     barcode = scrapy.Field()
     file_urls = scrapy.Field()
     files = scrapy.Field()
-    company_number = scrapy.Field()
     made_up_date = scrapy.Field()
+    filing_history_status = scrapy.Field()
--- filing_fetcher/spiders/latest_paper_filing.py
+++ filing_fetcher/spiders/latest_paper_filing.py
@@ -56,7 +56,25 @@ class LatestPaperFilingSpider(scrapy.spiders.CrawlSpider):
             )
 
     def parse_filing_history(self, response):
+
+        def set_if_present(item, name, data, path):
+            try:
+                item[name] = data[path]
+            except KeyError:
+                logger.warning(f"{path} not found in {data}")
+
+        filing_item = filing_fetcher.items.FilingItem()
+
+        set_if_present(filing_item, 'company_number', response.meta, 'CompanyNumber')
+        set_if_present(filing_item, 'company_status', response.meta, 'CompanyStatus')
+        set_if_present(filing_item, 'incorporation_date', response.meta, 'IncorporationDate')
+        set_if_present(filing_item, 'accounts_next_due_date', response.meta, 'Accounts.NextDueDate')
+        set_if_present(filing_item, 'accounts_last_made_up_date', response.meta, 'Accounts.LastMadeUpDate')
+        set_if_present(filing_item, 'accounts_account_category', response.meta, 'Accounts.AccountCategory')
+
         json_response = json.loads(response.body_as_unicode())
+
+        set_if_present(filing_item, 'filing_history_status', json_response, 'filing_history_status')
 
         if 'items' in json_response:
 
@@ -67,31 +85,18 @@ class LatestPaperFilingSpider(scrapy.spiders.CrawlSpider):
             if len(sorted_items) > 0:
                 most_recent_filing = sorted_items[0]
 
+                set_if_present(filing_item, 'pages', most_recent_filing, 'pages')
+                set_if_present(filing_item, 'date', most_recent_filing, 'date')
+                set_if_present(filing_item, 'type', most_recent_filing, 'type')
+                set_if_present(filing_item, 'barcode', most_recent_filing, 'barcode')
+
+                try:
+                    filing_item["made_up_date"] = most_recent_filing["description_values"]["made_up_date"]
+                except KeyError:
+                    pass
+
                 if 'paper_filed' in most_recent_filing and most_recent_filing['paper_filed']:
-                    paper_filing_item = filing_fetcher.items.FilingItem()
 
-                    paper_filing_item['company_number'] = response.meta['CompanyNumber']
-                    paper_filing_item['file_urls'] = [most_recent_filing['links']['document_metadata'] + '/content']
+                    filing_item['file_urls'] = [most_recent_filing['links']['document_metadata'] + '/content']
 
-                    paper_filing_item['pages'] = most_recent_filing['pages']
-                    paper_filing_item['date'] = most_recent_filing['date']
-                    paper_filing_item['type'] = most_recent_filing['type']
-                    paper_filing_item['barcode'] = most_recent_filing['barcode']
-
-                    try:
-                        paper_filing_item["made_up_date"] = most_recent_filing["description_values"]["made_up_date"]
-                    except KeyError:
-                        pass
-
-                    yield paper_filing_item
-                else:
-
-                    made_up_date = most_recent_filing["description_values"]["made_up_date"]
-                    logger.debug(f'No paper filing for {response.meta["CompanyNumber"]},'
-                                 f' date: {most_recent_filing["date"]}, made_up_date: {made_up_date}')
-
-        else:
-            try:
-                print(json_response['filing_history_status'])
-            except KeyError:
-                pass
+        yield filing_item
